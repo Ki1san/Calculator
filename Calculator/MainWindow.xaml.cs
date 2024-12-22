b4e8fb766766d628ef6a2cf67765e5bf4699937b
Neki1 <soevkirill696@gmail.com>
2024-12-22 12:56:20 +0300

    Добавьте файлы проекта.

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public double result = 0;
+
+        private void Display(TextBox Text)
+        {
+
+            string expression = TextBox.Text;
+            string[] parts = expression.Split(' ');
+
+            double a = double.Parse(parts[0]);
+            double b = double.Parse(parts[2]);
+            string operat = parts[1];
+
+            switch (operat)
+            {
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+                case "*":
+                    result = a * b;
+                    break;
+                case "/":
+                    if (b == 0)
+                    {
+                        MessageBox.Show("На 0 делить нельзя");
+                        return;
+                    }
+                    else
+                    {
+                        result = a / b;
+                        break;
+                    }
+                case "%":
+                    result = result / 100;
+                    break;
+
+                default:
+                    MessageBox.Show("Неверная операция");
+                    return;
+            }
+
+            Text.Text = result.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Display(TextBox);
+        }
+    }
+}
